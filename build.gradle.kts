b488857c6a76bf2c48de9da95153716c4caca8c9
jch0029987-glitch <jch0029987@gmail.com>
2026-09-07 11:56:24 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.23" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23" apply false
 }
